96c545adf9a9637eff3b882ad0f7c15d7b7685db
Vitai Tamás <vitaitamas@gmail.com>
2022-05-21 13:14:13 +0200

    Taking Photos

--- src/views/Tab2Page.vue
+++ src/views/Tab2Page.vue
@@ -11,6 +11,13 @@
           <ion-title size="large">Tab 2</ion-title>
         </ion-toolbar>
       </ion-header> -->
+      <ion-grid>
+        <ion-row>
+          <ion-col size="6" :key="photo" v-for="photo in photos">
+            <ion-img :src="photo.webviewPath"></ion-img>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
 
       <!-- <ExploreContainer name="Tab 2 page" /> -->
       <ion-fab vertical="bottom" horizontal="center" slot="fixed">
@@ -41,6 +48,7 @@ import {
   IonCol,
   IonImg,
 } from '@ionic/vue';
+import { usePhotoGallery, UserPhoto } from '@/composables/usePhotoGallery';
 
 export default defineComponent({
   name: 'Tab2Page',
@@ -60,7 +68,11 @@ export default defineComponent({
     IonImg,
   },
   setup() {
+    const { photos, takePhoto } = usePhotoGallery();
+
     return {
+      photos,
+      takePhoto,
       camera,
       trash,
       close,
