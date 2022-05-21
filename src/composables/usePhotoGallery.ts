d96a6e886d60955f7a74c4f17bd5b59fd920041c
Vitai Tamás <vitaitamas@gmail.com>
2022-05-21 13:20:30 +0200

    Saving Photos

--- src/composables/usePhotoGallery.ts
+++ src/composables/usePhotoGallery.ts
@@ -19,10 +19,11 @@ export function usePhotoGallery() {
     });
 
     const fileName = new Date().getTime() + ".jpeg";
-    const savedFileImage = {
-      filepath: fileName,
-      webviewPath: photo.webPath,
-    };
+    // const savedFileImage = {
+    //   filepath: fileName,
+    //   webviewPath: photo.webPath,
+    // };
+    const savedFileImage = await savePicture(photo, fileName);
 
     photos.value = [savedFileImage, ...photos.value];
   };
@@ -32,6 +33,41 @@ export function usePhotoGallery() {
     takePhoto,
   };
 }
+
+const convertBlobToBase64 = (blob: Blob) =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onerror = reject;
+    reader.onload = () => {
+      resolve(reader.result);
+    };
+    reader.readAsDataURL(blob);
+  });
+
+const savePicture = async (
+  photo: Photo,
+  fileName: string
+): Promise<UserPhoto> => {
+  let base64Data: string;
+
+  // Fetch the photo, read as a blob, then convert to base64 format
+  const response = await fetch(photo.webPath!);
+  const blob = await response.blob();
+  base64Data = (await convertBlobToBase64(blob)) as string;
+
+  const savedFile = await Filesystem.writeFile({
+    path: fileName,
+    data: base64Data,
+    directory: Directory.Data,
+  });
+
+  // Use webPath to display the new image instead of base64 since it's
+  // already loaded into memory
+  return {
+    filepath: fileName,
+    webviewPath: photo.webPath,
+  };
+};
 
 export interface UserPhoto {
   filepath: string;
